fb9992e6e38533deeaf1493311eaaa65c59e529a
Pedro Quiros <pedro.quiros@alumnos.frgp.utn.edu.ar>
2024-05-30 01:13:12 -0300

    Testeo datos de prueba

--- Coderhouse_Backend_PreEntrega1/src/routes/carts.routes.js
+++ Coderhouse_Backend_PreEntrega1/src/routes/carts.routes.js
@@ -3,14 +3,11 @@ import fs from "fs"
 
 const router = Router();
 
-/* La ruta raíz POST / deberá crear un nuevo carrito con la siguiente estructura:
-Id:Number/String (A tu elección, de igual manera como con los productos, debes asegurar que nunca se dupliquen los ids y que este se autogenere).
-products: Array que contendrá objetos que representen cada producto
-*/
 
 const products = JSON.parse(fs.readFileSync('./data/products.json', 'utf-8'));
 const carts = JSON.parse(fs.readFileSync('./data/carts.json', 'utf-8'));
 
+//La ruta raíz POST '/' deberá crear un nuevo carrito.
 router.post('/', (req, res) => {
     const newId = carts[carts.length -1].id + 1;
     const newCart = { 
@@ -23,14 +20,13 @@ router.post('/', (req, res) => {
     res.json(carts);
 })
 
-// La ruta GET /:cid deberá listar los productos que pertenezcan al carrito con el parámetro cid proporcionados.
-
+// La ruta GET '/:cid' deberá listar los productos que pertenezcan al carrito con el parámetro cid proporcionados.
 router.get('/:cid', (req, res) => {
     const { cid } = req.params;
 
     const cart = carts.find(cart => cart.id == cid);
     if (cid > carts[carts.length - 1].id || cid < 1 || !cart) {
-        res.status(400).json(`No se encuentra el carrito con el id: ${cid}, solicita uno entre el 1 y el ${carts[carts.length - 1].id}`)
+        res.status(400).json('id could not be found in the database');
     } else {
         try {
             res.json(cart);
@@ -40,17 +36,13 @@ router.get('/:cid', (req, res) => {
     }
 })
 
-/* La ruta POST  /:cid/product/:pid deberá agregar el producto al arreglo “products” del carrito seleccionado, agregándose como un objeto bajo el siguiente formato:
-product: SÓLO DEBE CONTENER EL ID DEL PRODUCTO (Es crucial que no agregues el producto completo)
-quantity: debe contener el número de ejemplares de dicho producto. El producto, de momento, se agregará de uno en uno.
-Además, si un producto ya existente intenta agregarse al producto, incrementar el campo quantity de dicho producto. */
-
+// La ruta POST  '/:cid/product/:pid' deberá agregar el producto al arreglo “products” del carrito seleccionado
 router.post('/:cid/product/:pid', (req, res) => {
     const { cid, pid } = req.params;
     
     const cart = carts.find(cart => cart.id == cid);
     const product = products.find(product => product.id == pid);
-    const isExistent = cart.products.find(product => product.product == pid);
+    const productExist = cart.products.find(product => product.product == pid);
 
     if (cid > carts[carts.length - 1].id || cid < 1 || !cart) {
         res.status(400).json(`No se encuentra el carrito con el id: ${cid}, solicita uno entre el 1 y el ${carts[carts.length - 1].id}`)
@@ -58,11 +50,15 @@ router.post('/:cid/product/:pid', (req, res) => {
         res.status(400).json(`No se encuentra el producto con el id: ${pid}, solicita uno entre el 1 y el ${products[products.length - 1].id}`)
     } else {
         try {
-            if (isExistent) {
-                isExistent.quantity += 1;
+            if (productExist) {
+                productExist.quantity += 1;
             } else {
-                cart.products.push({ product: product.id, quantity: 1 });
-            }
+                cart.products.push(
+                    { 
+                        product: product.id, 
+                        quantity: 1 }
+                    );
+                 }
             fs.writeFileSync('./data/carts.json', JSON.stringify(carts, null, '\t'));
             res.json(cart);
         } catch (err) {
--- Coderhouse_Backend_PreEntrega1/src/routes/products.routes.js
+++ Coderhouse_Backend_PreEntrega1/src/routes/products.routes.js
@@ -5,60 +5,70 @@ const router = Router();
 
 const products = JSON.parse(fs.readFileSync('./data/products.json', 'utf-8'));
 
-// La ruta raíz get deberá listar todos los productos de la base de datos.
+// La ruta raíz GET '/' deberá listar todos los productos de la base de datos.
 router.get('/', (req, res) => {
     res.json(products);
 })
 
-// La ruta GET /:pid deberá traer sólo el producto con el id proporcionado
+// La ruta GET '/:pid' deberá traer sólo el producto con el id proporcionado
 router.get('/:pid', async (req, res) => {
     const { pid } = req.params;
     const product = await products.find(product => product.id == pid);
 
     if(!product) {
-        res.status(404).json({ error: 'No se encuentra el producto con el id solicitado' })
+        res.status(404).json({ error: 'id could not be found in the database' });
     } else {
         res.json(product);
     }
 })
 
-// La ruta raíz POST / deberá agregar un nuevo producto a la base de datos.
+// La ruta raíz POST '/' deberá agregar un nuevo producto.
 router.post('/', (req, res) => {
     const { title, description, code, price, stock, category } = req.body;
     const newId = products[products.length - 1].id + 1;
 
-    if(!title || !description || !code || !price || !stock || !category) {
-        return res.status(400).json({ error: 'Todos los campos son obligatorios' })
-    } else {
-            const newProduct = {
-                id: newId,
-                title,
-                description,
-                code,
-                price,
-                status: true,
-                stock,
-                category
-            }
-
+    if( !title || 
+        !description ||
+        !code ||
+        !price || 
+        !stock ||
+        !category) {
+        return res.status(400).json({ error: 'All fields are required' });
+    } 
+    else {
+        const newProduct = {
+            id: newId,
+            title,
+            description,
+            code,
+            price,
+            status: true,
+            stock,
+            category
+         }
             products.push(newProduct);
             fs.writeFileSync('./data/products.json', JSON.stringify(products, null, '\t'));
         }
         res.json(products); 
-} 
-)
-// La ruta PUT /:pid deberá tomar un producto y actualizarlo por los campos enviados desde body. NUNCA se debe actualizar o eliminar el id al momento de hacer dicha actualización.
+    } )
+
+// La ruta PUT '/:pid' deberá tomar un producto y actualizarlo por los campos enviados desde body. NUNCA se debe actualizar o eliminar el id al momento de hacer dicha actualización.
 router.put('/:pid', (req, res) => {
     const { pid } = req.params;
     const { title, description, code, price, stock, category } = req.body;
 
-    if(!title || !description || !code || !price || !stock || !category) {
-        return res.status(400).json({ error: 'Todos los campos son obligatorios' })
+    if(!title || 
+       !description || 
+       !code || 
+       !price || 
+       !stock || 
+       !category) {
+        return res.status(400).json({ error: 'All fields are required' });
     } else {
         const product = products.find(product => product.id == pid);
 
         if(!product) {
-            res.status(404).json({ error: 'No se encuentra el producto con el id solicitado' })
+            res.status(404).json({ error: 'id could not be found in the database' });
         } else {
             product.title = title;
             product.description = description;
@@ -71,20 +81,22 @@ router.put('/:pid', (req, res) => {
         }
     }
 })
-// La ruta DELETE /:pid deberá eliminar el producto con el pid indicado.
+
+// La ruta DELETE '/:pid' deberá eliminar el producto con el pid indicado.
 router.delete('/:pid', async (req, res) => {
     const { pid } = req.params;
     const productIndex = products.findIndex(product => product.id == pid)
     
     if (pid > products[products.length - 1].id) {
-        res.status(400).json(`No se encuentra el producto con el id: ${pid} solicitado`);
+        res.status(400).json('id could not be found in the database');
     } else {
         try{
             const product = await products.splice(productIndex, 1)
             res.json(product);
         } catch(err) {
-            res.status(400).json(`Ocurrió un error al realizar la petición: ${err}`);
+            res.status(400).json('error deleting product');
         };
     }
 })
+
 export default router
